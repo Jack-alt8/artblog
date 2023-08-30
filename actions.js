a49c1845f970d321708015ad31493d14fbe71410
Jack Baylor <jackc.baylor@gmail.com>
2023-08-30 15:14:14 -0400

    fixing javascript

--- actions.js
+++ actions.js
@@ -1,6 +1,8 @@
-const toggleButton = document.getElementById('toggle-button');
-const naviList = document.getElementById('navi-list');
+const toggleButton = document.getElementById("toggle-button");
+const naviList = document.getElementById("navi-list");
 
-toggleButton.addEventListener('click', |) => {
-    naviList.classList.toggle('active');
-};
+toggleButton.addEventListener("click", () =>
+  naviList.classList.toggle("active")
+);
+
+//navButton.addEventListener('click', () => navLinks.classList.toggle('responsive'))
